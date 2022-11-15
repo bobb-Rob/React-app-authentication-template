108e68c469d3d155f0138f11bad65f06593b16f0
Robertson Arthur <robertsonakpan@gmail.com>
2022-11-15 11:28:57 +0100

    Authentication template done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,61 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Greeting from './Greeting';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './app/features/dashboard/Dashboard';
+import PrivateRoute from './app/features/routes/PrivateRoute';
+import PublicOnlyRoute from './app/features/routes/PublicRoute';
+import Login from './app/features/sessions/Login';
+import Logout from './app/features/sessions/Logout';
+import PersistLogin from './app/features/sessions/PersistLogin';
+import SignUp from './app/features/sessions/SignUp';
+import DataProvider from './app/DataProvider';
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Greeting />} />
-      </Routes>
-    </div>
+    <DataProvider>
+      <Router>
+        {/* <main className="grid md:grid-cols-desktop font-quicksand"> */}
+        <main>
+          <Routes>
+            <Route element={<PersistLogin />}>
+              <Route
+                path="/"
+                element={(
+                  <PrivateRoute>
+                    <Dashboard />
+                  </PrivateRoute>
+              )}
+              />
+              <Route
+                path="/logout"
+                element={(
+                  <PrivateRoute>
+                    <Logout />
+                  </PrivateRoute>
+                )}
+              />
+              <Route
+                path="/login"
+                element={(
+                  <PublicOnlyRoute>
+                    <Login />
+                  </PublicOnlyRoute>
+                )}
+              />
+              <Route
+                path="/signup"
+                element={(
+                  <PublicOnlyRoute>
+                    <SignUp />
+                  </PublicOnlyRoute>
+                )}
+              />
+            </Route>
+          </Routes>
+        </main>
+        {/* </main> */}
+      </Router>
+    </DataProvider>
   );
 }
+
 export default App;
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import greetingReducer from '../features/greetingSlice';
+import logger from 'redux-logger';
+import sessionReducer from './features/sessions/sessionSlice';
+// import rightDisplayReducer from '../redux/RSidebarReducers/RSidebarReducers';
+// import ItemListReducers from '../redux/ItemList/ItemListReducers';
+// import userReducer from '../redux/user/userSlice';
 
-export default configureStore({
+const store = configureStore({
   reducer: {
-    greeting: greetingReducer,
+    session: sessionReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
+
+export default store;
